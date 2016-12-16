c5f59852b2fd9558b1aed90372d7229dbdfee5d1
Flanker32 <liuhanxiaolcy@163.com>
2016-12-16 21:37:48 +0800

    修改：     src/main/java/edu/nju/software/Enterence.java

--- src/main/java/edu/nju/software/Enterence.java
+++ src/main/java/edu/nju/software/Enterence.java
@@ -25,82 +25,93 @@ public class Enterence {
 ////        small_world3.startDiffusion(startPoints,diffusionRound,startValue);
 //    }
 
-    public static void main(String[] args) {
-        Network scale_free0 = NetworkFactory.readNetworkFromFile("scale_free_nodes.txt", "scale_free_edges.txt", true, StrategyType.DetThr);
-//        Network scale_free1 = NetworkFactory.readNetworkFromFile("scale_free_nodes.txt","scale_free_edges.txt",true, StrategyType.DetAvg);
-//        Network scale_free2 = NetworkFactory.readNetworkFromFile("scale_free_nodes.txt","scale_free_edges.txt",true, StrategyType.ProThr);
-//        Network scale_free3 = NetworkFactory.readNetworkFromFile("scale_free_nodes.txt","scale_free_edges.txt",true, StrategyType.ProAvg);
+//    public static void main(String[] args) {
+//        Network scale_free0 = NetworkFactory.readNetworkFromFile("scale_free_nodes.txt", "scale_free_edges.txt", true, StrategyType.DetThr);
+////        Network scale_free1 = NetworkFactory.readNetworkFromFile("scale_free_nodes.txt","scale_free_edges.txt",true, StrategyType.DetAvg);
+////        Network scale_free2 = NetworkFactory.readNetworkFromFile("scale_free_nodes.txt","scale_free_edges.txt",true, StrategyType.ProThr);
+////        Network scale_free3 = NetworkFactory.readNetworkFromFile("scale_free_nodes.txt","scale_free_edges.txt",true, StrategyType.ProAvg);
+//
+//        Network random0 = NetworkFactory.readNetworkFromFile("random_nodes.txt", "random_edges.txt", true, StrategyType.DetThr);
+////        Network random1 = NetworkFactory.readNetworkFromFile("random_nodes.txt","random_edges.txt",true, StrategyType.DetAvg);
+////        Network random2 = NetworkFactory.readNetworkFromFile("random_nodes.txt","random_edges.txt",true, StrategyType.ProThr);
+////        Network random3 = NetworkFactory.readNetworkFromFile("random_nodes.txt","random_edges.txt",true, StrategyType.ProAvg);
+//
+//        Network regular0 = NetworkFactory.readNetworkFromFile("regular_nodes.txt", "regular_edges.txt", true, StrategyType.DetThr);
+////        Network regular1 = NetworkFactory.readNetworkFromFile("regular_nodes.txt","regular_edges.txt",true, StrategyType.DetAvg);
+////        Network regular2 = NetworkFactory.readNetworkFromFile("regular_nodes.txt","regular_edges.txt",true, StrategyType.ProThr);
+////        Network regular3 = NetworkFactory.readNetworkFromFile("regular_nodes.txt","regular_edges.txt",true, StrategyType.ProAvg);
+//
+//        Network small_world0 = NetworkFactory.readNetworkFromFile("small_world_nodes.txt", "small_world_edges.txt", true, StrategyType.DetThr);
+////        Network small_world1 = NetworkFactory.readNetworkFromFile("small_world_nodes.txt","small_world_edges.txt",true, StrategyType.DetAvg);
+////        Network small_world2 = NetworkFactory.readNetworkFromFile("small_world_nodes.txt","small_world_edges.txt",true, StrategyType.ProThr);
+////        Network small_world3 = NetworkFactory.readNetworkFromFile("small_world_nodes.txt","small_world_edges.txt",true, StrategyType.ProAvg);
+////        Network twitter = NetworkFactory.readNetworkFromFile("twitter_nodes.txt","twitter_edges.txt",false, StrategyType.DetThr);
+//        int startValue = 1;
+//        int diffusionRound = 100;
+//        int[] startPoints = {0, 1};
+//        System.out.println("网络生成完毕");
+//
+//        System.out.println("Scale_Free网络 D T");
+//        scale_free0.startDiffusion(startPoints, startValue, diffusionRound);
+////        System.out.println("Scale_Free网络 D A");
+////        scale_free1.startDiffusion(0.01,startValue,diffusionRound);
+////        System.out.println("Scale_Free网络 P T");
+////        scale_free2.startDiffusion(0.01,startValue,diffusionRound);
+////        System.out.println("Scale_Free网络 P A");
+////        scale_free3.startDiffusion(0.01,startValue,diffusionRound);
+//
+//        System.out.println("Random网络 D T");
+//        random0.startDiffusion(startPoints, startValue, diffusionRound);
+////        System.out.println("Random网络 D A");
+////        random1.startDiffusion(0.01,startValue,diffusionRound);
+////        System.out.println("Random网络 P T");
+////        random2.startDiffusion(0.01,startValue,diffusionRound);
+////        System.out.println("Random网络 P A");
+////        random3.startDiffusion(0.01,startValue,diffusionRound);
+//
+//        System.out.println("Regular网络 D T");
+//        regular0.startDiffusion(startPoints, startValue, diffusionRound);
+////        System.out.println("Regular网络 D A");
+////        regular1.startDiffusion(0.01,startValue,diffusionRound);
+////        System.out.println("Regular网络 P T");
+////        regular2.startDiffusion(0.01,startValue,diffusionRound);
+////        System.out.println("Regular网络 P A");
+////        regular3.startDiffusion(0.01,startValue,diffusionRound);
+//
+//        System.out.println("Small_world网络 D T");
+//        small_world0.startDiffusion(startPoints, startValue, diffusionRound);
+////        System.out.println("Small_world网络 D A");
+////        small_world1.startDiffusion(0.01,startValue,diffusionRound);
+////        System.out.println("Small_world网络 P T");
+////        small_world2.startDiffusion(0.01,startValue,diffusionRound);
+////        System.out.println("Small_world网络 P A");
+////        small_world3.startDiffusion(0.01,startValue,diffusionRound);
+//
+//    }
 
-        Network random0 = NetworkFactory.readNetworkFromFile("random_nodes.txt", "random_edges.txt", true, StrategyType.DetThr);
-//        Network random1 = NetworkFactory.readNetworkFromFile("random_nodes.txt","random_edges.txt",true, StrategyType.DetAvg);
-//        Network random2 = NetworkFactory.readNetworkFromFile("random_nodes.txt","random_edges.txt",true, StrategyType.ProThr);
-//        Network random3 = NetworkFactory.readNetworkFromFile("random_nodes.txt","random_edges.txt",true, StrategyType.ProAvg);
+        public static void main(String[] args) {
+            NetworkParameter networkParameter = new NetworkParameter();
+            networkParameter.setBinary(false);
+            networkParameter.setNetworkType(NetworkType.ScaleFree);
+            networkParameter.setStrategyType(StrategyType.DetAvg);
+            networkParameter.setAgentNumber(2500);
+            networkParameter.setEdgeNumber(10000);
+            Network network = NetworkFactory.generateNetwork(networkParameter);
+            network.startDiffusion(0.01,10,100);
 
-        Network regular0 = NetworkFactory.readNetworkFromFile("regular_nodes.txt", "regular_edges.txt", true, StrategyType.DetThr);
-//        Network regular1 = NetworkFactory.readNetworkFromFile("regular_nodes.txt","regular_edges.txt",true, StrategyType.DetAvg);
-//        Network regular2 = NetworkFactory.readNetworkFromFile("regular_nodes.txt","regular_edges.txt",true, StrategyType.ProThr);
-//        Network regular3 = NetworkFactory.readNetworkFromFile("regular_nodes.txt","regular_edges.txt",true, StrategyType.ProAvg);
-
-        Network small_world0 = NetworkFactory.readNetworkFromFile("small_world_nodes.txt", "small_world_edges.txt", true, StrategyType.DetThr);
-//        Network small_world1 = NetworkFactory.readNetworkFromFile("small_world_nodes.txt","small_world_edges.txt",true, StrategyType.DetAvg);
-//        Network small_world2 = NetworkFactory.readNetworkFromFile("small_world_nodes.txt","small_world_edges.txt",true, StrategyType.ProThr);
-//        Network small_world3 = NetworkFactory.readNetworkFromFile("small_world_nodes.txt","small_world_edges.txt",true, StrategyType.ProAvg);
-//        Network twitter = NetworkFactory.readNetworkFromFile("twitter_nodes.txt","twitter_edges.txt",false, StrategyType.DetThr);
-        int startValue = 1;
-        int diffusionRound = 100;
-        int[] startPoints = {0, 1};
-        System.out.println("网络生成完毕");
-
-        System.out.println("Scale_Free网络 D T");
-        scale_free0.startDiffusion(startPoints, startValue, diffusionRound);
-//        System.out.println("Scale_Free网络 D A");
-//        scale_free1.startDiffusion(0.01,startValue,diffusionRound);
-//        System.out.println("Scale_Free网络 P T");
-//        scale_free2.startDiffusion(0.01,startValue,diffusionRound);
-//        System.out.println("Scale_Free网络 P A");
-//        scale_free3.startDiffusion(0.01,startValue,diffusionRound);
-
-        System.out.println("Random网络 D T");
-        random0.startDiffusion(startPoints, startValue, diffusionRound);
-//        System.out.println("Random网络 D A");
-//        random1.startDiffusion(0.01,startValue,diffusionRound);
-//        System.out.println("Random网络 P T");
-//        random2.startDiffusion(0.01,startValue,diffusionRound);
-//        System.out.println("Random网络 P A");
-//        random3.startDiffusion(0.01,startValue,diffusionRound);
-
-        System.out.println("Regular网络 D T");
-        regular0.startDiffusion(startPoints, startValue, diffusionRound);
-//        System.out.println("Regular网络 D A");
-//        regular1.startDiffusion(0.01,startValue,diffusionRound);
-//        System.out.println("Regular网络 P T");
-//        regular2.startDiffusion(0.01,startValue,diffusionRound);
-//        System.out.println("Regular网络 P A");
-//        regular3.startDiffusion(0.01,startValue,diffusionRound);
-
-        System.out.println("Small_world网络 D T");
-        small_world0.startDiffusion(startPoints, startValue, diffusionRound);
-//        System.out.println("Small_world网络 D A");
-//        small_world1.startDiffusion(0.01,startValue,diffusionRound);
-//        System.out.println("Small_world网络 P T");
-//        small_world2.startDiffusion(0.01,startValue,diffusionRound);
-//        System.out.println("Small_world网络 P A");
-//        small_world3.startDiffusion(0.01,startValue,diffusionRound);
-
-    }
-
+        }
 //    public static void main(String[] args) {
 //        NetworkParameter networkParameter = new NetworkParameter();
 //        networkParameter.setBinary(true);
 //        networkParameter.setStrategyType(StrategyType.DetThr);
-
+//
 //        Network dCluster = NetworkFactory.readSouthEaseNetworkFromFile(30000,100,"cluster.txt",networkParameter);
 //        Network dRandom = NetworkFactory.readSouthEaseNetworkFromFile(30000,100,"random.txt",networkParameter);
 //        Network dScaleFree = NetworkFactory.readSouthEaseNetworkFromFile(30000,100,"scalefree.txt",networkParameter);
 //        Network dSmallWorld = NetworkFactory.readSouthEaseNetworkFromFile(30000,100,"smallworld.txt",networkParameter);
-
-
-    //        System.out.println("dCluster diffusion!");
+//
+//
+//            System.out.println("dCluster diffusion!");
 //    double total = 0;
 //        for(int i=0;i<100;i++){
 //            Network dCluster = NetworkFactory.readSouthEaseNetworkFromFile(30000,100,"cluster.txt",networkParameter);
@@ -117,7 +128,7 @@ public class Enterence {
 //        dScaleFree.startDiffusion(startList,1,100);
 //        System.out.println("dSmallWorld diffusion!");
 //        dSmallWorld.startDiffusion(startList,1,100);
-
+//
 //}
 
 
